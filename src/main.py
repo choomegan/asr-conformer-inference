dd4ba8a993f2832a8bc280be6199ea392fcb237f
Megan <choomegan@gmail.com>
2024-02-23 10:47:28 +0800

    feat: main script for fastapi calls

---
+++ src/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from src.asr import ASRService
+
+api = FastAPI()
+service = ASRService()
+
+
+class TranscribeRequest(BaseModel):
+    audio_filepath: str | None = None
+
+
+@api.get("/")
+def root():
+    """
+    Test if API endpoint works
+    """
+    return {"response": "200", "message": "API service to do ASR with conformer"}
+
+
+@api.post("/transcribe")
+def transcribe(request: TranscribeRequest):
+    print(request.json())
+    return service.transcribe(request.audio_filepath)
